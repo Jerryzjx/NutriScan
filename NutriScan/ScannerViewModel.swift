977d90a1ec37a5bb6de3bb95c1e82388fa0eb21f
JerryZhang0920 <jinxuan0920@gmail.com>
2024-02-04 23:06:03 -0500

    Update ScannerViewModel.swift

--- NutriScan/ScannerViewModel.swift
+++ NutriScan/ScannerViewModel.swift
@@ -31,7 +31,8 @@ final class ScannerViewModel: ObservableObject {
     @Published var dataScannerAccessStatus: DataScannerAccessStatusType = .notDetermined
     @Published var recognizedData: [RecognizedItem] = []
     @Published var scanType: ScanType = .barcode
- 
+    @Published var itemDetails: [String] = []
+    
     private var isScannerAvaliable: Bool {
         DataScannerViewController.isAvailable && DataScannerViewController.isSupported
     }
@@ -64,5 +65,28 @@ final class ScannerViewModel: ObservableObject {
         }
         
     }
+    
+    func fetchDataForScannedBarcode(_ barcode: String) {
+            Task {
+                do {
+                    let fetchedItem = try await APIManager.shared.searchItem(with: barcode)
+                    DispatchQueue.main.async {
+                        // Update your itemDetails or recognizedData here based on the API response
+                        self.itemDetails.append("Fetched item detail: \(fetchedItem)") // Simplified for demonstration
+                    }
+                } catch {
+                    print("Error fetching item details: \(error)")
+                }
+            }
+        }
+
+        // Assuming you have a function to process recognized items
+        func processRecognizedItems() {
+            for item in recognizedData {
+                if case .barcode(let barcode) = item, let barcodeValue = barcode.payloadStringValue {
+                    fetchDataForScannedBarcode(barcodeValue)
+                }
+            }
+        }
     
 }
