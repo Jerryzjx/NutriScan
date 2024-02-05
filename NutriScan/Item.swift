684e3181cf0b27151e65a905eaacb08a3fadd47e
JerryZhang0920 <jinxuan0920@gmail.com>
2024-02-05 14:01:44 -0500

    Updated SwiftData Model

--- NutriScan/Item.swift
+++ NutriScan/Item.swift
@@ -11,12 +11,62 @@ import SwiftData
 @Model
 class Item {
     var timestamp: Date
-    var name: String
-    var brandName: String
+        var foodName: String
+        var brandName: String
+        var servingQty: Int?
+        var servingUnit: String?
+        var servingWeightGrams: Double?
+        var calories: Double?
+        var totalFat: Double?
+        var saturatedFat: Double?
+        var cholesterol: Double?
+        var sodium: Double?
+        var totalCarbohydrate: Double?
+        var dietaryFiber: Double?
+        var sugars: Double?
+        var protein: Double?
+        var potassium: Double?
+        var phosphorus: Double? // Representing nfP
     
-    init(timestamp: Date, name: String, brandName: String) {
+    init(timestamp: Date, name: String, brandName: String, servingQty: Int? = nil, servingUnit: String? = nil, servingWeightGrams: Double? = nil, calories: Double? = nil, totalFat: Double? = nil, saturatedFat: Double? = nil, cholesterol: Double? = nil, sodium: Double? = nil, totalCarbohydrate: Double? = nil, dietaryFiber: Double? = nil, sugars: Double? = nil, protein: Double? = nil, potassium: Double? = nil, phosphorus: Double? = nil) {
         self.timestamp = timestamp
-        self.name = name
+        self.foodName = name
         self.brandName = brandName
+        self.servingQty = servingQty
+        self.servingUnit = servingUnit
+        self.servingWeightGrams = servingWeightGrams
+        self.calories = calories
+        self.totalFat = totalFat
+        self.saturatedFat = saturatedFat
+        self.cholesterol = cholesterol
+        self.sodium = sodium
+        self.totalCarbohydrate = totalCarbohydrate
+        self.dietaryFiber = dietaryFiber
+        self.sugars = sugars
+        self.protein = protein
+        self.potassium = potassium
+        self.phosphorus = phosphorus
+    }
+    
+    convenience init(from foodItem: FoodItem, timestamp: Date = Date()) {
+        self.init(
+                    timestamp: timestamp,
+                    name: foodItem.foodName,
+                    brandName: foodItem.brandName ?? "",
+                    servingQty: foodItem.servingQty,
+                    servingUnit: foodItem.servingUnit,
+                    servingWeightGrams: foodItem.servingWeightGrams,
+                    calories: foodItem.nfCalories,
+                    totalFat: foodItem.nfTotalFat,
+                    saturatedFat: foodItem.nfSaturatedFat,
+                    cholesterol: foodItem.nfCholesterol,
+                    sodium: foodItem.nfSodium,
+                    totalCarbohydrate: foodItem.nfTotalCarbohydrate,
+                    dietaryFiber: foodItem.nfDietaryFiber,
+                    sugars: foodItem.nfSugars,
+                    protein: foodItem.nfProtein,
+                    potassium: foodItem.nfPotassium,
+                    phosphorus: foodItem.nfP
+        )
     }
 }
