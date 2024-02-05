0ea8b8cd96ade8bb1169d3ebdf3e4373f024df3d
JerryZhang0920 <jinxuan0920@gmail.com>
2024-02-04 23:06:10 -0500

    Update DataScannerView.swift

--- NutriScan/DataScannerView.swift
+++ NutriScan/DataScannerView.swift
@@ -12,6 +12,7 @@ import VisionKit
 struct DataScannerView: UIViewControllerRepresentable {
     
     @Binding var recognizedData: [RecognizedItem]
+    @EnvironmentObject var viewModel: ScannerViewModel
     
     //let recognizedDataTypes: DataScannerViewController.RecognizedDataType
     // let recognizeMultipleItems: Bool
@@ -35,7 +36,7 @@ struct DataScannerView: UIViewControllerRepresentable {
     }
     
     func makeCoordinator() -> Coordinator {
-        Coordinator(recognizedData: $recognizedData)
+        Coordinator(recognizedData: $recognizedData, viewModel: viewModel)
     }
     
     static func dismantleUIViewController(_ uiViewController: DataScannerViewController, coordinator: Coordinator) {
@@ -45,9 +46,11 @@ struct DataScannerView: UIViewControllerRepresentable {
     class Coordinator: NSObject, DataScannerViewControllerDelegate {
         
         @Binding var recognizedData: [RecognizedItem]
+        var viewModel: ScannerViewModel
         
-        init(recognizedData: Binding<[RecognizedItem]>) {
+        init(recognizedData: Binding<[RecognizedItem]>, viewModel: ScannerViewModel) {
             self._recognizedData = recognizedData
+            self.viewModel = viewModel
         }
         
         func dataScanner(_ dataScanner: DataScannerViewController, didTapOn item: RecognizedItem) {
@@ -56,18 +59,28 @@ struct DataScannerView: UIViewControllerRepresentable {
         
         func dataScanner(_ dataScanner: DataScannerViewController, didAdd addedItems: [RecognizedItem], allItems: [RecognizedItem]) {
             UINotificationFeedbackGenerator().notificationOccurred(.success)
-            recognizedData.append(contentsOf: addedItems)
             print("didAdd: \(addedItems)")
+            
+            for item in addedItems {
+                    if case let .barcode(barcode) = item, let barcodeValue = barcode.payloadStringValue {
+                        // Use the existing function in the ViewModel to process the barcode
+                        // Assuming you have a way to access the ViewModel instance here. If not, you'll need to pass it to the Coordinator.
+                        self.viewModel.fetchDataForScannedBarcode(barcodeValue)
+
+                        // Now that fetching is delegated to the ViewModel, you might reconsider if you need to immediately remove the barcode from recognizedData here
+                        // If removal is necessary, ensure it's handled appropriately in context with your app's flow
+                    }
+                }
         }
-        
-        func dataScanner(_ dataScanner: DataScannerViewController, didRemove removedItems: [RecognizedItem], allItems: [RecognizedItem]) {
-            print("didRemove: \(removedItems)")
-        }
-        
-        func dataScanner(_ dataScanner: DataScannerViewController, becameUnavailableWithError error: DataScannerViewController.ScanningUnavailable) {
-            print("becameUnavailableWithError: \(error.localizedDescription)")
-        }
-        
+    
+    func dataScanner(_ dataScanner: DataScannerViewController, didRemove removedItems: [RecognizedItem], allItems: [RecognizedItem]) {
+        print("didRemove: \(removedItems)")
     }
     
+    func dataScanner(_ dataScanner: DataScannerViewController, becameUnavailableWithError error: DataScannerViewController.ScanningUnavailable) {
+        print("becameUnavailableWithError: \(error.localizedDescription)")
+    }
+    
+}
+
 }
