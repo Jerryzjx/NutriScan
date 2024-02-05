5dad5eaae52c629bc58563e53d406d77e3d2d3b6
JerryZhang0920 <jinxuan0920@gmail.com>
2024-02-04 23:06:05 -0500

    Update NutriScanApp.swift

--- NutriScan/NutriScanApp.swift
+++ NutriScan/NutriScanApp.swift
@@ -15,9 +15,9 @@ struct NutriScanApp: App {
     
     var sharedModelContainer: ModelContainer = {
         let schema = Schema([
-            Item.self,
+            Item.self
         ])
-        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+        let modelConfiguration = ModelConfiguration(schema: schema)
 
         do {
             return try ModelContainer(for: schema, configurations: [modelConfiguration])
