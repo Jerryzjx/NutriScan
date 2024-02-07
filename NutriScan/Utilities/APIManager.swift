a4b66276057e602d8a65d9d775eaba29795ea32d
JerryZhang0920 <jinxuan0920@gmail.com>
2024-02-07 12:09:54 -0500

    Create APIManager.swift

---
+++ NutriScan/Utilities/APIManager.swift
@@ -0,0 +1,40 @@
+//
+//  APIManager.swift
+//  NutriScan
+//
+//  Created by leonard on 2024-02-04.
+//
+
+import Foundation
+import Alamofire
+
+
+
+class APIManager {
+    
+    static let shared = APIManager()
+    
+    private let baseURL = "https://trackapi.nutritionix.com/v2/search/item"
+    
+    func searchItem(with barcode: String) async throws -> [FoodItem] {
+        let headers: HTTPHeaders = [
+            "Content-Type": "application/json",
+            "x-app-id": "ID",
+            "x-app-key": "KEY",
+        ]
+        
+        let parameters: [String: String] = ["upc": barcode]
+        
+        do {
+            let response = try await AF.request(baseURL, method: .get, parameters: parameters, headers: headers)
+                .validate()
+                .serializingDecodable(FoodsResponse.self)
+                .value
+            
+            return response.foods // Return the array of FoodItem
+        } catch {
+            throw error
+        }
+    }
+
+    }
