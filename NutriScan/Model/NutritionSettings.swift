20b75a49148cef5fd1cbbd0b236eea30f56fc167
JerryZhang0920 <jinxuan0920@gmail.com>
2024-02-09 21:36:23 -0500

    Create NutritionSettings.swift

---
+++ NutriScan/Model/NutritionSettings.swift
@@ -0,0 +1,17 @@
+//
+//  NutritionSettings.swift
+//  NutriScan
+//
+//  Created by leonard on 2024-02-09.
+//
+
+import Foundation
+
+struct NutritionConstants {
+    static let calories = 2250.0
+    static let fat = 75.0
+    static let carbs = 100.0
+    static let protein = 50.0
+}
+
+let username = "Leonard"
